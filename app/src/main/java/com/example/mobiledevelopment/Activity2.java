1c70d83341e81c2b597adea455804d3866cfc61d
malyalaprateeprao <prateepraomalyala@gmail.com>
2022-05-28 22:17:47 -0400

    display viewtext when button clicks

--- app/src/main/java/com/example/mobiledevelopment/Activity2.java
+++ app/src/main/java/com/example/mobiledevelopment/Activity2.java
@@ -3,6 +3,10 @@ package com.example.mobiledevelopment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +14,58 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+
+    public void onClickA(View v) {
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+
+        changeTextOnce(buttonText, b);
+    }
+
+    public void onClickB(View v) {
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+
+        changeTextOnce(buttonText, b);
+    }
+
+    public void onClickC(View v) {
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+
+        changeTextOnce(buttonText, b);
+    }
+
+    public void onClickD(View v) {
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+
+        changeTextOnce(buttonText, b);
+    }
+
+    public void onClickE(View v) {
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+
+        changeTextOnce(buttonText, b);
+    }
+
+    public void onClickF(View v) {
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+
+        changeTextOnce(buttonText, b);
+    }
+
+    public void changeTextOnce(String buttonText, Button changeTextButton){
+        final TextView changingText = (TextView) findViewById(R.id.textView);
+
+        changeTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                changingText.setText("Pressed: -" + buttonText);
+            }
+        });
     }
 }
